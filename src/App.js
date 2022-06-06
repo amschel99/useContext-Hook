a0eaea483b3e80e5a51844157e7e6942108840b8
amschell <100097727+amschell@users.noreply.github.com>
2022-06-06 11:35:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  const[time, setTime]=useState(new Date())
+  useEffect(
+    ()=>{
+     const timer= setTimeout(()=>{setTime(new Date())},1000) 
+     return ()=>{clearTimeout(timer)}
+    },[time]
+  )
+  return <>
+  <h1>MADE BY AMSCHEL: {time.toLocaleTimeString()}</h1>
+  </>
+  
+  
 }
-
 export default App;
+
